1cc9867253da121f0617ec607ef14f73d1c023cc
J0S39H-MC <josephmcclean@outlook.com>
2020-07-14 23:29:52 -0500

    abstract oracle dbConnection and dbCommand from OracleDataAccessor.

--- gRPCService101/DataAccessor/BaseDataAccessor.cs
+++ gRPCService101/DataAccessor/BaseDataAccessor.cs
@@ -10,22 +10,32 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 
+
 namespace gRPCService101.DataAccessor
 {
     public class BaseDataAccessor
     {
-        protected IDbConnection DbConnection { get; set; }
+        private readonly DbCommand dbCommand;
 
-        public BaseDataAccessor(IConfiguration configuration, string connectionStringName)
+        protected virtual DbConnection DbConnection { get; set; }
+
+        public BaseDataAccessor(Func<DbConnection> dbConnectionFunc, Func<DbCommand> dbCommandFunc)
+        {
+            this.DbConnection = dbConnectionFunc?.Invoke();
+            this.dbCommand = dbCommandFunc.Invoke();
+        }
+
+        public BaseDataAccessor(IConfiguration configuration, string connectionStringName, DbCommand dbCommand)
         {
             var connectionString = configuration?.GetConnectionString(connectionStringName);
             this.DbConnection = new OracleConnection(connectionString);
+            this.dbCommand = dbCommand;
         }
 
-        protected virtual async Task<IEnumerable<T>> ExecuteCommandQuery<T>(IDbConnection conn,
-                                                                       string qryString,
-                                                                       CommandType commandType = CommandType.StoredProcedure,
-                                                                       List<DbParameter> @params = null) where T : new()
+        protected virtual async Task<IEnumerable<T>> ExecuteCommandQuery<T>(DbConnection conn,
+                                                                            string qryString,
+                                                                            CommandType commandType = CommandType.StoredProcedure,
+                                                                            List<DbParameter> @params = null) where T : new()
         {
             return await ExecuteQuery<T>(conn, qryString, commandType, @params);
         }
@@ -46,42 +56,44 @@ namespace gRPCService101.DataAccessor
             return qryresults;
         }
 
-        protected virtual async Task<IEnumerable<T>> ExecuteQuery<T>(IDbConnection connection, string qryString, CommandType commandType, List<DbParameter> @params) where T : new()
+        protected virtual async Task<IEnumerable<T>> ExecuteQuery<T>(DbConnection connection, string qryString, CommandType commandType, List<DbParameter> @params) where T : new()
         {
             // check db connection
             // check command qry string
             if (connection == null)
                 throw new Exception("connection is null");
-
+            
             List<T> genericObjects = new List<T>();
-            OracleCommand command = new OracleCommand();
-            command.Connection = connection as OracleConnection;
+            DbCommand command = connection.CreateCommand();
+            command.Connection = connection;
             command.CommandText = qryString;
             command.CommandType = commandType;
             @params?.ForEach((param) => command.Parameters.Add(param));
 
             using (connection)
             {
-                if (connection.State == ConnectionState.Closed)
-                    connection.Open();
+                if (connection.State == ConnectionState.Closed) connection.Open();
 
                 var asyncReader = (await command.ExecuteReaderAsync());
 
                 using (asyncReader)
                 {
-                    while (await asyncReader.ReadAsync())
+                    using (this.dbCommand)
                     {
-                        T genericObject = new T();
-                        var databaseColumns = typeof(T).GetProperties().Where(p => Attribute.IsDefined(p, typeof(ColumnAttribute))).ToList();
-                        databaseColumns.ForEach((property) =>
+                        while (await asyncReader.ReadAsync())
                         {
-                            ColumnAttribute attribute = property.GetCustomAttribute<ColumnAttribute>();
-                            var value = asyncReader[attribute.Name];
-                            var converter = TypeDescriptor.GetConverter(property.PropertyType);
-                            var convertedValue = converter.ConvertTo(value, property.PropertyType);
-                            property.SetValue(genericObject, convertedValue);
-                        });
-                        genericObjects.Add(genericObject);
+                            T genericObject = new T();
+                            var databaseColumns = typeof(T).GetProperties().Where(p => Attribute.IsDefined(p, typeof(ColumnAttribute))).ToList();
+                            databaseColumns.ForEach((property) =>
+                            {
+                                ColumnAttribute attribute = property.GetCustomAttribute<ColumnAttribute>();
+                                var value = asyncReader[attribute.Name];
+                                var converter = TypeDescriptor.GetConverter(property.PropertyType);
+                                var convertedValue = converter.ConvertTo(value, property.PropertyType);
+                                property.SetValue(genericObject, convertedValue);
+                            });
+                            genericObjects.Add(genericObject);
+                        }
                     }
                 }
             }
--- gRPCService101/DataAccessor/OracleDataAccessor.cs
+++ gRPCService101/DataAccessor/OracleDataAccessor.cs
@@ -2,6 +2,8 @@
 using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +11,10 @@ namespace gRPCService101.DataAccessor
 {
     public class OracleDataAccessor : BaseDataAccessor
     {
-        public OracleDataAccessor(IConfiguration configuration) : base(configuration, "OracleConnection") { }
+        public OracleDataAccessor(IConfiguration configuration) : base(() => { return new OracleConnection(configuration?.GetConnectionString("OracleConnection")); }, () => { return new OracleCommand(); })
+        //: base(configuration, "OracleConnection", new OracleCommand())
+        {
+
+        }
     }
 }
